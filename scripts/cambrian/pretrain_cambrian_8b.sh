d8bdab8daa9f4b2b6967bb75849ad5246c054881
Natyren <bredis.georgy@yandex.ru>
2024-09-04 20:46:13 +0300

    .

--- scripts/cambrian/pretrain_cambrian_8b.sh
+++ scripts/cambrian/pretrain_cambrian_8b.sh
@@ -33,11 +33,11 @@ python cambrian/train/train_tpu.py \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --image_aspect_ratio pad \
-    --bf16 False \
+    --bf16 True \
     --output_dir $CKPT_DIR \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 2 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
